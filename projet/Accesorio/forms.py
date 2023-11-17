b244b0a05dbb090d7798bc0fa87fbef8f21b55a6
Moreno <106407303+MMorenoMM19999@users.noreply.github.com>
2023-11-17 15:12:49 -0500

    Delete projet directory

--- projet/Accesorio/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Carro
-
-class CarroForm(forms.ModelForm):
-    class Meta:
-        model = Carro
-        fields = ['marca', 'modelo', 'accesorios']  # incluye 'accesorios' en el formulario
-        
